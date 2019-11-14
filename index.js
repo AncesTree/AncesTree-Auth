03893b4a0d314c97ed3f85eff49a15eea84993a0
Thibaut <Thibaut.allard-serre@etu.umontpellier.fr>
2019-11-14 12:52:58 +0100

    Updated cors

--- index.js
+++ index.js
@@ -8,13 +8,6 @@ const cors = require('cors');
 const app = express()
 app.use(bodyParser.json())
 app.use(cors());
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
 
 const oauth = require("./routes/oauth")
 const invitation = require("./routes/invitation")
