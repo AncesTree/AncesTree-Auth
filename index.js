3e9059eedb0331a2a8058dde4d5c809762016962
Thibaut <Thibaut.allard-serre@etu.umontpellier.fr>
2019-11-14 12:28:27 +0100

    Added cors

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require("express")
 const request = require("sync-request")
 const qs = require("qs")
 require('dotenv').config()
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
+const cors = require('cors');
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 const oauth = require("./routes/oauth")
 const invitation = require("./routes/invitation")
